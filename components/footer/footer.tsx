285be6345d6a5fa73c6fff85bfff287859341b9a
JeanCRuiz <139879016+JeanCRuiz@users.noreply.github.com>
2024-06-20 16:59:19 -0400

    updating

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,9 +1,8 @@
-import { Outfit } from 'next/font/google';
+import { Outfit } from "next/font/google";
+import Link from "next/link";
+import styles from "./footer.module.css";
 
-import Link from 'next/link';
-import styles from './footer.module.css';
-
-const outfit = Outfit({ subsets: ['latin'], weight: ['700'] });
+const outfit = Outfit({ subsets: ["latin"], weight: ["700"] });
 function Footer() {
   return (
     <footer className={`app-container ${styles.footer}`}>
@@ -11,56 +10,56 @@ function Footer() {
 
       <nav className={styles.navegation}>
         <Link
-          href='https://www.linkedin.com/in/jeancarlosruiz'
-          target='_blank'
+          href="https://www.linkedin.com/in/jeancarlosruiz"
+          target="_blank"
           replace
           className={styles.link}
         >
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='16'
-            height='16'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            role='img'
-            aria-labelledby='linkedin-icon'
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            role="img"
+            aria-labelledby="linkedin-icon"
           >
-            <title id='linkedin-icon'>Jean Ruiz&apos;s LinkedIn profile</title>
-            <path d='M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z'></path>
-            <rect x='2' y='9' width='4' height='12'></rect>
-            <circle cx='4' cy='4' r='2'></circle>
+            <title id="linkedin-icon">Jean Ruiz&apos;s LinkedIn profile</title>
+            <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+            <rect x="2" y="9" width="4" height="12"></rect>
+            <circle cx="4" cy="4" r="2"></circle>
           </svg>
           LinkedIn
         </Link>
         <small>|</small>
         <Link
-          href='https://github.com/jeancarlosruiz'
-          target='_blank'
+          href="https://github.com/jeancarlosruiz"
+          target="_blank"
           replace
           className={styles.link}
         >
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='16'
-            height='16'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            role='img'
-            aria-labelledby='github-icon'
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            role="img"
+            aria-labelledby="github-icon"
           >
-            <title id='github-icon'>Jean Ruiz&apos;s Github profile</title>
+            <title id="github-icon">Jean Ruiz&apos;s GitHub profile</title>
 
-            <path d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'></path>
+            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
           </svg>
-          Github
+          GitHub
         </Link>
       </nav>
     </footer>

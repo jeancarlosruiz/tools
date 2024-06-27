9c0a9a2aa1eb334d476320d1c4c08aa7e36d4e10
Jean Carlos Ruiz <66891780+jeancarlosruiz@users.noreply.github.com>
2024-06-27 06:59:49 -0400

    bug fixed

--- components/clampConvertion/clampConvertion.tsx
+++ components/clampConvertion/clampConvertion.tsx
@@ -2,7 +2,6 @@
 import { useEffect, useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Button } from '@/components/ui/button';
 import { toast } from 'sonner';
 import copy from 'copy-to-clipboard';
 import styles from './clampConvertion.module.css';
@@ -157,7 +156,7 @@ const ClampConvertion = () => {
         </Label>
         <CustomInput
           inputValue={maxWidth}
-          inputOnchange={(e) => setMaxWidthUnit(e.target.value)}
+          inputOnchange={(e) => setMaxWidth(e.target.value)}
           selectValue={maxWidthUnit}
           selectOnchange={(e) => {
             setMaxWidthUnit(e);
@@ -172,7 +171,7 @@ const ClampConvertion = () => {
         <CustomInput
           inputValue={maxFontsize}
           inputOnchange={(e) => setMaxFontsize(e.target.value)}
-          selectValue={maxWidthUnit}
+          selectValue={maxFontsizeUnit}
           selectOnchange={(e) => {
             setMaxFontsizeUnit(e);
             const newValue = switchValueFn(e, maxFontsize);
@@ -182,31 +181,6 @@ const ClampConvertion = () => {
         />
       </div>
       <CodeResult clamp={clamp} />
-      {/* <div className={styles.resultWrapper}>
-        <Label htmlFor='result-input'>
-          <strong>Font-size:</strong>
-        </Label>
-        <div className={styles.clampResultWrapper}>
-          <span className={styles.clampResult}>{clamp}</span>
-          <Button
-            size='icon'
-            className={`${styles.copyBtn} ${styles.copyIcon}`}
-            onClick={copyToClipboard}
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 16 16'
-              width='20'
-              height='20'
-              role='img'
-              className={styles.svg}
-            >
-              <path d='M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z'></path>
-              <path d='M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z'></path>
-            </svg>
-          </Button>
-        </div>
-      </div> */}
     </section>
   );
 };
